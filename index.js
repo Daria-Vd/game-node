65c87daf1a8d0b4a57e9c2dfe6b0286275539f3c
Dariya Vedernikova <daria-vedernikova08@yandex.ru>
2024-04-06 20:12:50 +0300

    init: start project

---
+++ index.js
@@ -0,0 +1,9 @@
+require("events").EventEmitter.defaultMaxListeners = 0;
+const dictionary = require("./AppModules/dictionary");
+// Подключите лаунчер, который создали
+
+const launcher = require("./AppModules/launcher");
+
+console.log(dictionary.global.hello);
+
+launcher.run();
